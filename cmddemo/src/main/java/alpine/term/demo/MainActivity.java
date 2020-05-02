673a773355f0c64dc410640e1ac4f43b3b043ecf
Matthew Good <smallville7123@gmail.com>
2020-05-02 10:55:48 +1000

    simplified client application a bit

--- cmddemo/src/main/java/alpine/term/demo/MainActivity.java
+++ cmddemo/src/main/java/alpine/term/demo/MainActivity.java
@@ -16,22 +16,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e(Config.APP_LOG_TAG, "AM I PRINTING? 0");
-        int pseudoTerminal = TerminalController.createPseudoTerminal();
-        TerminalControllerService terminalControllerService = new TerminalControllerService();
-        Log.e(Config.APP_LOG_TAG, "AM I PRINTING? 1");
-        terminalControllerService.bindToTerminalService(this);
-        Log.e(Config.APP_LOG_TAG, "AM I PRINTING? 2");
-        terminalControllerService.registerActivity(this, pseudoTerminal);
-        Log.e(Config.APP_LOG_TAG, "AM I PRINTING? 3");
-        new Thread() {
-            @Override
-            public void run() {
-                while(true) {
-                    SystemClock.sleep(1000);
-                    Log.e(Config.APP_LOG_TAG, "AM I PRINTING? LOOPED");
-                }
-            }
-        }.start();
+        TerminalControllerService TCS = new TerminalControllerService();
+        TCS.bindToTerminalService(this);
+        TCS.registerActivity(this, TerminalController.createPseudoTerminal());
     }
 }
